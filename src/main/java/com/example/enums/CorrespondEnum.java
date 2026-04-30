bd8e025d5c794304969077439818052efd475dbf
BigBigPrince <2197725569@qq.com>
2026-04-30 11:10:08 +0800

    feat: 添加Java项目模板文件及更新对应枚举路径
    添加多个Java项目模板文件，包括异常处理、响应枚举、Redis配置、服务启动类等
    更新CorrespondEnum中的模板文件路径，将路径从java/改为java_single/

--- src/main/java/com/example/enums/CorrespondEnum.java
+++ src/main/java/com/example/enums/CorrespondEnum.java
@@ -3,14 +3,14 @@ package com.example.enums;
 import com.example.CodeGenerator;
 
 public enum CorrespondEnum {
-    GlobalVo("/entity/vo/GlobalVo.java", "java/globalVo.java.ftl", ">>> 通用VO文件已生成"),
-    Global_Exception("/exception/GlobalException.java", "java/exception.java.ftl", ">>> GlobalException 已生成"),
-    Global_Exception_Handler("/exception/GlobalExceptionHandler.java", "java/globalExceptionHandler.java.ftl", ">>> GlobalExceptionHandler 已生成"),
-    Response_Enums("/entity/enums/ResponseEnums.java", "java/responseEnums.java.ftl", ">>> 通用响应式枚举文件已生成"),
-    Redis_Config("/redis/RedisConfig.java", "java/redisConfig.java.ftl", ">>> Redis_Config 已完成"),
-    Redis_Service("/redis/RedisService.java", "java/redisService.java.ftl", ">>> Redis_Service 已完成"),
-    Init_Run("/InitRun.java", "java/InitRun.java.ftl", ">>> Init_Run 已完成"),
-    ApplicationGenerator("/" + CodeGenerator.applicationName + "Application.java", "java/application.java.ftl", ">>> Application.java 已完成");
+    GlobalVo("/entity/vo/GlobalVo.java", "java_single/globalVo.java.ftl", ">>> 通用VO文件已生成"),
+    Global_Exception("/exception/GlobalException.java", "java_single/exception/exception.java.ftl", ">>> GlobalException 已生成"),
+    Global_Exception_Handler("/exception/GlobalExceptionHandler.java", "java_single/exception/globalExceptionHandler.java.ftl", ">>> GlobalExceptionHandler 已生成"),
+    Response_Enums("/entity/enums/ResponseEnums.java", "java_single/responseEnums.java.ftl", ">>> 通用响应式枚举文件已生成"),
+    Redis_Config("/redis/RedisConfig.java", "java_single/redis/redisConfig.java.ftl", ">>> Redis_Config 已完成"),
+    Redis_Service("/redis/RedisService.java", "java_single/redis/redisService.java.ftl", ">>> Redis_Service 已完成"),
+    Init_Run("/InitRun.java", "java_single/InitRun.java.ftl", ">>> Init_Run 已完成"),
+    ApplicationGenerator("/" + CodeGenerator.applicationName + "Application.java", "java_single/application.java.ftl", ">>> Application.java 已完成");
 
 
     CorrespondEnum(String filePath, String fileName, String finishLog) {
